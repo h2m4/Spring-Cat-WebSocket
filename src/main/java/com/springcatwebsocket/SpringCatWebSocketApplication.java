180116a60761ddbe8440fc081e114eff2644040c
hiumei <hiu2mei4@gmail.com>
2022-03-17 07:08:43 -0400

    init commit

---
+++ src/main/java/com/springcatwebsocket/SpringCatWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.springcatwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCatWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcatwebsocket/SpringCatWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcatwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
